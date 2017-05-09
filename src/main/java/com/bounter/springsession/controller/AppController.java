d90458ab40ddf16d933be6f0c39194c852432e4e
26.5°C <1317492210@qq.com>
2017-05-09 15:35:02 +0800

    初始化项目

---
+++ src/main/java/com/bounter/springsession/controller/AppController.java
@@ -0,0 +1,30 @@
+package com.bounter.springsession.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by admin on 2017/4/16.
+ */
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+	
+	@RequestMapping("/put")
+    public void put(HttpSession session) {
+        session.setAttribute("name", "simon");
+    }
+	
+	@RequestMapping("/get")
+	@ResponseBody
+    public String get(HttpSession session) {
+		return (String) session.getAttribute("name");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>hello, Spring Session !</h2>
+</body>
+</html>
